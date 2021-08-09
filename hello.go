ff823bdaddd68ec876982446146dd902e1986a28
p.lysak <p.lysak@cian.ru>
2021-08-09 22:18:44 +0300

    master -- my first test

--- hello.go
+++ hello.go
@@ -1,9 +1,11 @@
 package main
 
 import "fmt"
+import "github.com/pulysak/go_hello_world/morestrings"
 
 import "rsc.io/quote"
 
 func main() {
 	fmt.Println(quote.Go())
+	fmt.Println(morestrings.ReverseRunes("!oG, olleH"))
 }
